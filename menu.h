5aebb5f7d1e0adcd0805b9909bc479985313ca61
FerranSalvat <ferry96@hotmail.es>
2018-05-16 17:58:09 +0200

    Menu

---
+++ menu.h
@@ -0,0 +1,12 @@
+//
+// Created by Ferran Salvat on 16/5/18.
+//
+
+#ifndef BLACKJACK_MENU_H
+#define BLACKJACK_MENU_H
+
+int menuPrincipal();
+int menuJugar();
+int menuEstadisticas();
+
+#endif //BLACKJACK_MENU_H
